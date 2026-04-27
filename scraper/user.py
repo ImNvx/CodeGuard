cb70190d81c83d12893c277d1f2037704df2039a
eric991 <ericmesteraga@proton.me>
2026-04-27 17:35:56 +0000

    o sa fac datele de antrenament

--- scraper/user.py
+++ scraper/user.py
@@ -2,7 +2,7 @@ import json
 import pandas as pd
 from io import StringIO
 
-USER = 'BatinCatalin'
+USERS = ['eric.mester', 'buzdi', 'EricTiberiuFrancu', 'LucaLucaM ', 'ASN49K', 'laura', 'atodo']
 
 def read_first_n(file_path, le, ri):
     data = []
@@ -12,16 +12,16 @@ def read_first_n(file_path, le, ri):
                 break
             if i >= le:
                 x = json.loads(line)
-                if(len(data) == 0 and x['user'] == USER and x['score'] == '100') :
+                if(len(data) == 0 and x['user'] in USERS and x['score'] == '100') :
                     data.append(x)
-                elif(x['user'] == USER and x['score'] == '100' and x['code'] != data[-1]['code']):
+                elif(x['user'] in USERS and x['score'] == '100' and x['code'] != data[-1]['code']):
                     data.append(x)
     return data
 
 def goy(text):
     return text.replace('\n',  ' ')
 
-data_json = json.dumps(read_first_n('kilonova.json', 1, 50000))
+data_json = json.dumps(read_first_n('kilonova.json', 1, 1000000))
 
 df = pd.read_json(StringIO(data_json))
 df['code'] = df['code'].apply(goy)
