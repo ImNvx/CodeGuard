a60af7f387cc6a0045ab4a06dbd5be09a76c72cf
nvx <113138273+ImNvx@users.noreply.github.com>
2026-05-05 16:15:58 +0300

    Update CodeGuard_AI.py

--- AI/CodeGuard_AI.py
+++ AI/CodeGuard_AI.py
@@ -7,7 +7,7 @@ class CodeGuard(): # am facut clasa pana la urma ca daca era functie simpla treb
     def __init__(self):
         self.tokenizer = AutoTokenizer.from_pretrained("AI/CodeGuard_tokenizer")
         self.model = CodeGuardEncoder(vocab_size=self.tokenizer.vocab_size)
-        self.model.load_state_dict(torch.load("AI/CodeGuard.pth"), map_location=torch.device('cpu'))
+        self.model.load_state_dict(torch.load("AI/CodeGuard.pth", map_location=torch.device('cpu')))
 
     def checkSubmission(self, previous_submissions, current_submission):
         with torch.no_grad():
