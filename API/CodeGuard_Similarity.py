b5704139189bf37ca93bd1cd6e09edac17e05dc6
eric991 <ericmesteraga@proton.me>
2026-05-22 19:51:09 +0000

    oopsie

--- API/CodeGuard_Similarity.py
+++ API/CodeGuard_Similarity.py
@@ -34,7 +34,7 @@ def normalize_tokens(code: str):
     return normalized
 
 
-def shingles(tokens, k=5):
+def shingles(tokens, k=3):
     # daca sunt prea putine token-uri
     if len(tokens) < k:
         return {" ".join(tokens)}
