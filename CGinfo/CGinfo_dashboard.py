be6e2a4f5f945e65d8a975ebd29374d26f6964bb
nvx <113138273+ImNvx@users.noreply.github.com>
2026-06-18 19:47:50 +0300

    Added buttons and created the class that will be used in the DB

--- CGinfo/CGinfo_dashboard.py
+++ CGinfo/CGinfo_dashboard.py
@@ -1,9 +1,32 @@
 import flet as ft
 
+class Clasa:
+    def __init__(self, nivel=9, nume="A", nr_elevi=0):
+        self.nivel = nivel
+        self.nume = nume
+        self.nr_elevi = nr_elevi
+
+class ButonClasa(ft.Button):
+    def __init__(self, label_text):
+        super().__init__(content=ft.Text(label_text, size=50),)
+        
+        self.bgcolor = ft.Colors.BLUE_GREY_100
+        self.color = ft.Colors.BLACK
+        self.width = 300
+        self.height = 120
+        self.style = ft.ButtonStyle(
+            shape=ft.RoundedRectangleBorder(radius=8)
+        )
+
 def main(page: ft.Page):
+
+    clase = [Clasa(nr_elevi=28), Clasa(nivel=10, nume="B", nr_elevi=22), Clasa(nivel=11, nume="C", nr_elevi=25)] # aici o sa facem sa le ia din db mai tarziu (tinyDB pare smecher)
+
     page.title = "CGinfo"
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.vertical_alignment = ft.MainAxisAlignment.START
     page.theme_mode = ft.ThemeMode.DARK
+
+    class_buttons = [ButonClasa(label_text=f"{i.nivel}{i.nume}") for i in clase]
 
     page.add(
         ft.Stack(
@@ -20,7 +43,12 @@ def main(page: ft.Page):
                     padding=20,
                 )
             ],
-            expand=True
+            height=200
+        ),
+        ft.Row(
+            controls=class_buttons, # iar sa le fac sa fie maxim 3 pe row
+            alignment=ft.MainAxisAlignment.CENTER,
+            spacing=25
         )
     )
 
