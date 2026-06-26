7fb9faf68c460e2b3e52f678e4fef6969dd63394
nvx <113138273+ImNvx@users.noreply.github.com>
2026-06-27 02:55:51 +0300

    Did even more refactoring, added back routing methods for navigating

--- CGinfo/CGinfo_dashboard.py
+++ CGinfo/CGinfo_dashboard.py
@@ -4,58 +4,12 @@ from tinydb import TinyDB, Query
 import sqlite3
 from CGinfo_methods import *
 
-def add_clasa(e, page : ft.Page):
-    page.title = "Adăugare clasă"
-
-
-def main_page(page, clase, class_buttons):
-    page.add(
-        ft.Stack(
-            controls=[
-                ft.Container(
-                    content=ft.Text("CGinfo Dashboard", size=35, weight=ft.FontWeight.BOLD),
-                    alignment=ft.Alignment.TOP_LEFT,
-                    padding=15,
-                ),
-                
-                ft.Container(
-                    content=ft.Image(src="cg_shield.png", width=125),
-                    alignment=ft.Alignment.TOP_CENTER,
-                    padding=20,
-                )
-            ],
-            height=200
-        ),
-    )
-
-    for i in range(0, len(clase), 3):
-        page.add(
-            ft.Row(
-                controls=class_buttons[i:i+3],
-                alignment=ft.MainAxisAlignment.CENTER,
-                spacing=25
-            )
-        )
-    page.add(
-        ft.Row(
-            controls=[ButonAddClasa("Adăugă o clasă nouă", page=page)],
-            alignment=ft.MainAxisAlignment.CENTER,
-        )
-    )
-    #sa fac padding adjustment pe window resize
-
 def main(page: ft.Page):
     db = TinyDB('db_clase.json')
     clase = [Clasa(**i) for i in db.all()]
-
     clase.sort(key=lambda k : (k.nivel, k.nume))
 
-    page.title = "CGinfo"
-    page.vertical_alignment = ft.MainAxisAlignment.START
-    page.theme_mode = ft.ThemeMode.DARK
-
-    class_buttons = [ButonClasa(clasa=i, page=page) for i in clase]
-
-    main_page(page, clase, class_buttons)
+    mp = MainPage(page = page, clase=clase)
+    mp.loadPage()
 
 ft.run(main)
--- CGinfo/CGinfo_methods.py
+++ CGinfo/CGinfo_methods.py
@@ -20,8 +20,23 @@ class Elev:
         self.nume = nume
         self.istoric_submisii = istoric_submisii
 
+class ButonBack(ft.IconButton):
+    def __init__(self, dest):
+        super().__init__()
+
+        self.bgcolor = ft.Colors.BLUE_GREY_800
+        self.style= ft.ButtonStyle(
+            shape=ft.RoundedRectangleBorder(radius=18)
+        )
+        self.icon = ft.Icon(
+            ft.Icons.ARROW_BACK,
+            size=30
+        )
+        self.width = 60
+        self.on_click = lambda e : dest()
+
 class ButonClasa(ft.Button):
-    def __init__(self, clasa : Clasa, page : ft.Page):
+    def __init__(self, clasa : Clasa, page : ft.Page, back_route):
         super().__init__(content=ft.Text(f"{clasa.nivel}{clasa.nume}", size=50),)
         
         self._page = page
@@ -34,14 +49,20 @@ class ButonClasa(ft.Button):
             shape=ft.RoundedRectangleBorder(radius=12)
         )
         self.on_click = self.class_selection
+        self.back_route = back_route
 
     def class_selection(self, e):
         self._page.clean()
         self._page.title = f"Clasa {self.clasa.nivel}{self.clasa.nume}"
+        self._page.add(
+            ft.Row(
+                ButonBack(dest=self.back_route)
+            )
+        )
         self._page.update()
 
 class ButonAddClasa(ft.Button):
-    def __init__(self, label_text, page : ft.Page):
+    def __init__(self, label_text, page : ft.Page, back_route):
         super().__init__(content=ft.Text(label_text, size=15, text_align=ft.TextAlign.CENTER),)
         
         self._page = page
@@ -57,8 +78,61 @@ class ButonAddClasa(ft.Button):
             shape=ft.RoundedRectangleBorder(radius=16)
         )
         self.on_click = self.add_clasa
+        self.back_route = back_route
 
     def add_clasa(self, e):
         self._page.clean()
         self._page.title = "Adăugare clasă"
+        self._page.add(
+            ft.Row(
+                ButonBack(dest=self.back_route)
+            )
+        )
         self._page.update()
+
+class MainPage():
+    def __init__(self, page : ft.Page, clase):
+        self._page = page
+        self.clase = clase
+        self.class_buttons = [ButonClasa(clasa=i, page=self._page, back_route=self.loadPage) for i in self.clase]
+        
+    def loadPage(self):
+        self._page.clean()
+        self._page.title = "CGinfo"
+        self._page.vertical_alignment = ft.MainAxisAlignment.START
+        self._page.theme_mode = ft.ThemeMode.DARK
+        self._page.update()
+
+        self._page.add(
+        ft.Stack(
+            controls=[
+                ft.Container(
+                    content=ft.Text("CGinfo Dashboard", size=35, weight=ft.FontWeight.BOLD),
+                    alignment=ft.Alignment.TOP_LEFT,
+                    padding=15,
+                ),
+                
+                ft.Container(
+                    content=ft.Image(src="cg_shield.png", width=125),
+                    alignment=ft.Alignment.TOP_CENTER,
+                    padding=20,
+                )
+            ],
+            height=200
+        ),
+        )
+
+        for i in range(0, len(self.clase), 3):
+            self._page.add(
+                ft.Row(
+                    controls=self.class_buttons[i:i+3],
+                    alignment=ft.MainAxisAlignment.CENTER,
+                    spacing=25
+                )
+            )
+        self._page.add(
+            ft.Row(
+                controls=[ButonAddClasa("Adăugă o clasă nouă", page=self._page, back_route=self.loadPage)],
+                alignment=ft.MainAxisAlignment.CENTER,
+            )
+        ) #sa fac padding adjustment pe window resize
