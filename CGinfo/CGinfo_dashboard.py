db469e2eb35572b220ef9286db56c1a891eed1fd
nvx <113138273+ImNvx@users.noreply.github.com>
2026-06-30 23:56:54 +0300

    Created multiple button classes, modified page min size and scroll, refactored some code and defined some methods

--- CGinfo/CGinfo_dashboard.py
+++ CGinfo/CGinfo_dashboard.py
@@ -4,10 +4,15 @@ from tinydb import TinyDB, Query
 import sqlite3
 from CGinfo_methods import *
 
+
 def main(page: ft.Page):
     db = TinyDB('db_clase.json')
     clase = [Clasa(**i) for i in db.all()]
     clase.sort(key=lambda k : (k.nivel, k.nume))
+
+    page.window.min_width = 1000
+    page.window.min_height = 600
+    page.scroll = ft.ScrollMode.AUTO
 
     mp = MainPage(page = page, clase=clase)
     mp.loadPage()
--- CGinfo/CGinfo_methods.py
+++ CGinfo/CGinfo_methods.py
@@ -1,4 +1,10 @@
 import flet as ft
+from tinydb import TinyDB, Query
+import sqlite3
+
+
+def get_elev_submissions(kn_user): #returneaza toate submisiile unui elev din db
+    return "int main()"
 
 class Clasa:
     def __init__(self, nivel=9, nume="A", nr_elevi=0, elevi=[]):
@@ -16,9 +22,17 @@ class Clasa:
         }
 
 class Elev:
-    def __init__(self, nume, istoric_submisii):
+    def __init__(self, nume, kn_user, id): #id = get_kn_id(kn_user)
         self.nume = nume
-        self.istoric_submisii = istoric_submisii
+        #self.istoric_submisii = istoric_submisii -> asta o sa existe doar in baza de date si dam query pe kn_user, nu il tinem in memorie
+        self.kn_user = kn_user
+        self.id = 0
+
+class Submisie:
+    def __init__(self, content, id, user):
+        self.content = content
+        self.id = id
+        self.user = user
 
 class ButonBack(ft.IconButton):
     def __init__(self, dest):
@@ -34,6 +48,15 @@ class ButonBack(ft.IconButton):
         )
         self.width = 60
         self.on_click = lambda e : dest()
+
+class ButonElev(ft.Button):
+    def __init__(self, nume):
+        super().__init__(content=ft.Text(nume, size=20))
+        self.width = 300
+        self.height = 45
+        self.style = ft.ButtonStyle(
+            shape=ft.RoundedRectangleBorder(radius=15)
+        )
 
 class ButonClasa(ft.Button):
     def __init__(self, clasa : Clasa, page : ft.Page, back_route):
@@ -54,16 +77,41 @@ class ButonClasa(ft.Button):
     def class_selection(self, e):
         self._page.clean()
         self._page.title = f"Clasa {self.clasa.nivel}{self.clasa.nume}"
+
         self._page.add(
             ft.Row(
                 ButonBack(dest=self.back_route)
             )
         )
+        
+        db = TinyDB("db_clase.json")
+        q = Query()
+        res = db.search((q.nivel == self.clasa.nivel) & (q.nume == self.clasa.nume)) # aceste 2 conditii ar trebui sa fie suficiente parerea mea
+        
+        elevi = [Elev(**e) for e in res[0]["elevi"]]
+        elevi.sort(key = lambda k : k.nume)
+
+
+        for i in elevi:
+            self._page.add(
+                ft.Row(
+                    controls=ButonElev(nume=i.nume),
+                    alignment=ft.MainAxisAlignment.CENTER,
+                )
+            )
+
+        # adaugare lista cu elevii aici pe linia asta
+        self._page.add(
+            ft.Row(
+                controls=[ButonAddElev(page = self._page, back_route=self.back_route)],
+                alignment=ft.MainAxisAlignment.CENTER,
+            )
+        )
         self._page.update()
 
 class ButonAddClasa(ft.Button):
-    def __init__(self, label_text, page : ft.Page, back_route):
-        super().__init__(content=ft.Text(label_text, size=15, text_align=ft.TextAlign.CENTER),)
+    def __init__(self, page : ft.Page, back_route):
+        super().__init__(content=ft.Text("Adaugă o clasă nouă", size=15, text_align=ft.TextAlign.CENTER),)
         
         self._page = page
         self.bgcolor = ft.Colors.TEAL_200
@@ -89,7 +137,35 @@ class ButonAddClasa(ft.Button):
             )
         )
         self._page.update()
+    
 
+class ButonAddElev(ft.Button):
+    def __init__(self, page : ft.Page, back_route):
+        super().__init__(content=ft.Text("Adaugă un elev", size=15, text_align=ft.TextAlign.CENTER),)
+        
+        self._page = page
+        self.bgcolor = ft.Colors.TEAL_200
+        self.color = ft.Colors.BLACK
+        self.width = 200
+        self.height = 50
+        self.icon = ft.Icon(
+            ft.Icons.ADD,
+            size=25,
+        )
+        self.style = ft.ButtonStyle(
+            shape=ft.RoundedRectangleBorder(radius=16)
+        )
+        self.on_click = self.add_elev
+        self.back_route = back_route
+    
+    def add_elev(self, e):
+        self._page.clean()
+        self._page.title = "Adăugare elev"
+        
+        self._page.update()
+
+# !!!! neaparat sa fac elementele sa isi dea resize in functie de window size, probabil ca event 
+# pe window resize si sa fie un procent din size-ul curent sau ceva multiplier in functie de size
 class MainPage():
     def __init__(self, page : ft.Page, clase):
         self._page = page
@@ -132,7 +208,7 @@ class MainPage():
             )
         self._page.add(
             ft.Row(
-                controls=[ButonAddClasa("Adăugă o clasă nouă", page=self._page, back_route=self.loadPage)],
+                controls=[ButonAddClasa(page=self._page, back_route=self.loadPage)],
                 alignment=ft.MainAxisAlignment.CENTER,
             )
-        ) #sa fac padding adjustment pe window resize
+        )
